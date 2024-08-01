25ebe952f80b0da223bc0717979ebb168228536f
pnv28 <pnv.khemka@gmail.com>
2024-08-01 14:01:31 +0530

    chore: Update favicon link in index.html and remove unused pickup.css file

--- src/pages/pickup.js
+++ src/pages/pickup.js
@@ -1,16 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './css/pickup.css'
 
 const Pickup = () => {
     return (
         <div>
-            <div class="navbar">
-                Soon TO Be you know
-            </div>
-            <div class="container">
-                    <iframe title="Pickup Form" src="https://tally.so/embed/mBBWN1?alignLeft=1&transparentBackground=1" class="form" frameBorder="0"></iframe>
-            </div>
+            
         </div>
     );
 };
